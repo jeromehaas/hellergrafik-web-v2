2642e0cba2742895cef65581dd35a83ee3fe6807
jeromehaas <hello@jeromehaas.ch>
2023-01-03 03:59:34 +0100

    Setup: add small-arrow-link component

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,13 @@
 import Text from "components/01-atoms/text/text";
 import Heading from "components/01-atoms/heading/heading";
+import SmallArrowLink from "components/01-atoms/small-arrow-link/small-arrow-link";
 
 const Start = () => {
 
   return (
     <div className="start">
 			<Heading>Heading</Heading>
+			<SmallArrowLink>Link</SmallArrowLink>
 		</div>
   );
 
