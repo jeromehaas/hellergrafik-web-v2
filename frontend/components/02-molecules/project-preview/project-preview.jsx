38c420cc05575a0342d8ab2596d1e164a90a778d
jeromehaas <hello@jeromehaas.ch>
2023-01-04 06:06:52 +0100

    Setup: add showcase component

--- frontend/components/02-molecules/project-preview/project-preview.jsx
+++ frontend/components/02-molecules/project-preview/project-preview.jsx
@@ -3,25 +3,28 @@ import Image from "next/image";
 import Link from "next/link";
 import PropTypes  from "prop-types";
 
-const ProjectPreview = ({ className }) => {
+const ProjectPreview = ({ className, title, image, href }) => {
 
 	return (
 		<div className={`${ className } project-preview`}>
-			<Link className="project-preview__image-link" href="/">
-				<Image className="project-preview__image" src="/images/projects/hellergrafik/01-hellergrafik.jpg" width="2500" height="2000" alt="Project Image" />
+			<Link className="project-preview__image-link" href={ href }>
+				<Image className="project-preview__image" src={ image.src } width="2500" height="2000" alt={ image.alt } />
 			</Link>
-			<BigArrowLink className="project-preview__link">Project Link</BigArrowLink>
+			<BigArrowLink className="project-preview__link" href={ href }>{ title }</BigArrowLink>
 		</div>
 	);
 
 };
 
 ProjectPreview.propTypes = {
-	className: PropTypes.string
+	className: PropTypes.string,
+	title: PropTypes.string,
+	image: PropTypes.object
 };
 
 ProjectPreview.defaultProps = {
-	className: ''
+	className: '',
+	title: '',
 };
 
 export default ProjectPreview;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,7 @@
 import Page from "components/04-layouts/page/page";
 import Hero from "components/03-organisms/hero/hero";
 import Intro from "components/03-organisms/intro/intro";
+import Showcase from "components/03-organisms/showcase/showcase";
 
 const Start = () => {
 
@@ -8,6 +9,7 @@ const Start = () => {
 		<Page>
 			<Hero />
 			<Intro />
+			<Showcase />
 		</Page>
   );
 
