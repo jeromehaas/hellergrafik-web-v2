397c32c50d448cfc9303cd3d90ae2d935d5e5240
jeromehaas <hello@jeromehaas.ch>
2023-02-08 08:20:10 +0100

    Setup: 3-card grid layout

--- frontend/components/01-atoms/big-arrow-link/big-arrow-link.jsx
+++ frontend/components/01-atoms/big-arrow-link/big-arrow-link.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 
-const BigArrowLink = ({ className, children }) => {
+const BigArrowLink = ({ className, children, href }) => {
 
 	return (
-		<a className={`${ className } big-arrow-link`}>
+		<a className={`${ className } big-arrow-link`} href={ href }>
 			<span className="big-arrow-link__text">{ children }</span>
 			<svg className="big-arrow-link__arrow arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 17">
 				<polygon className="arrow__vector" points="13.95 8.46 12.54 7.05 12.5 7.09 5.46 0.05 4.05 1.46 10.09 7.5 0 7.5 0 9.5 10.09 9.5 4.05 15.54 5.46 16.95 12.5 9.91 12.54 9.95 13.95 8.54 13.91 8.5 13.95 8.46"/>
--- frontend/components/02-molecules/picture/picture.jsx
+++ frontend/components/02-molecules/picture/picture.jsx
@@ -2,9 +2,22 @@ import Image from "next/image";
 import PropTypes from 'prop-types';
 
 const Picture = ({ className, images, priority }) => {
+
+	
+	const getLength = (images) => {
+		let length;
+		switch(images.length) {
+			case 1: length = 'one'; break;
+			case 2: length = 'two'; break;
+			case 3: length = 'three'; break;
+			default: 'one';
+		};
+		return length;
+	};
+	
 	
 	return (
-		<div className={`${ className } picture`}>
+		<div className={`${ className } picture picture--${ getLength(images) }`}>
 			{ images.map((image, index) => (
 				<Image className="picture__item" priority={ priority }  key={ index } src={ image.src } width={ 2500 } height={ image.orientation === 'portrait' ? 2500 : 2950 } alt={ image.alt }  />
 			))}
