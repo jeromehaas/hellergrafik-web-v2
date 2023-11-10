591921df14545ff40f7d3698747fa9b13fc59855
jeromehaas <hello@jeromehaas.ch>
2023-11-10 20:01:14 +0100

    Refactor: layout of project-detail images

--- frontend/components/02-molecules/picture/picture.jsx
+++ frontend/components/02-molecules/picture/picture.jsx
@@ -3,21 +3,8 @@ import PropTypes from 'prop-types';
 
 const Picture = ({ className, images, priority }) => {
 
-	
-	const getLength = (images) => {
-		let length;
-		switch(images.length) {
-			case 1: length = 'one'; break;
-			case 2: length = 'two'; break;
-			case 3: length = 'three'; break;
-			default: 'one';
-		};
-		return length;
-	};
-	
-	
 	return (
-		<div className={`${ className } picture picture--${ getLength(images) }`}>
+		<div className={`${ className } picture`}>
 			{ images.map((image, index) => (
 				<Image className="picture__item" priority={ priority }  key={ index } src={ image.src } width={ 2500 } height={ image.orientation === 'portrait' ? 2500 : 2950 } alt={ image.alt }  />
 			))}
